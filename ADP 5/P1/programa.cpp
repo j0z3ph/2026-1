7a73726390d9cbb573c1a8c2d15484aaecd8b09c
Jose Luis Cruz <j0z3ph@gmail.com>
2025-08-28 14:26:37 -0600

    Codigo

---
+++ ADP 5/P1/programa.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file programa.cpp
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C++ :>
+ * @version 0.1
+ * @date 2025-08-28
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int edad = 10;
+    string nombre;
+
+    cout << "Usuario, ingresa tu nombre completo: ";
+    getline(cin, nombre);
+
+    cout << "Usuario, ingresa tu edad: ";
+
+    cin >> edad;
+
+    cout << nombre << ", tu edad es " << edad << "." << endl;
+
+    return 0;
+}
