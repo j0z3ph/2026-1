d5c43c0b0ad6afe4512163e2debce7f6a6344beb
j0z3ph <j0z3ph@gmail.com>
2025-08-28 12:44:49 -0600

    Added example

---
+++ PA 4/P1/prog.py
@@ -0,0 +1,45 @@
+print("Hola")
+
+# Comentario 
+
+'''
+kjdhkfjsd 
+dbkhjfhdsf
+kjdhfjkhds
+
+'''
+
+variable = 5
+variable = "Hola"
+variable = False
+CONSTANTE = 7
+CONSTANTE = 8
+
+lista = [1, 1, True, "hola", 'a', 6.7]
+#lista[0] = 9
+print(lista)
+
+tupla = (1, 1, True, "hola", 'a', 6.7)
+#tupla[0] = 10
+print(tupla)
+
+diccionario = {'llave':7, 'otro':True, 'Mas': 'Cadena'}
+print(diccionario['otro'])
+
+conjunto = {1,1,1,1,1,1,2}
+print(conjunto)
+
+a = '1'
+b = '2'
+print(int(a) + int(b))
+
+cadena = "Holi Canoli"
+
+var = 5.6789
+
+cad = "El valor es {0:^10.2f}"
+
+
+print(cad.format(var))
+
+print(f"El valor es {var:^10.2f}")
