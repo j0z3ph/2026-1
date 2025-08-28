8995e789460830b029321b5552270d79e1ad5dda
Jose Luis Cruz <j0z3ph@gmail.com>
2025-08-27 18:52:18 -0600

    Primer programa

---
+++ ADP 9/P1/programa.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file programa.cpp
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C++ :D
+ * @version 0.1
+ * @date 2025-08-27
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int edad;
+    cout << "Usuario, ingresa tu edad: ";
+    cin >> edad;
+
+    cout << "Tu edad es " << edad << "." << endl;
+    return 0;
+}
