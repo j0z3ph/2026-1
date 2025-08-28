2e87c35fe1a077656b1e1c1151f6ab70e2737ed7
Jose Luis Cruz <j0z3ph@gmail.com>
2025-08-28 15:59:05 -0600

    Ejemplos

---
+++ PA 6/P1/prog2.py
@@ -0,0 +1,3 @@
+valor1 = int(input("Usuario, ingresa el primer valor: "))
+valor2 = int(input("Usuario, ingresa el segundo valor: "))
+print(f"La suma es {valor1 + valor2}.")
---
+++ PA 6/P1/programa.py
@@ -0,0 +1,56 @@
+# Esto es un comentario
+
+'''
+Esto tambien
+es un comentario
+'''
+
+"""
+Otro comen
+tario
+"""
+
+
+variable = 7
+variable = 7.8
+variable = "siete punto ocho"
+CONSTANTE = 3.1416
+
+lista = [1,2,3,"4", "cinco", True, False, 5.6]
+print(lista)
+lista[2] = "HOLI"
+print(lista)
+
+tupla = (1,2,3,"Hola", False)
+print(tupla)
+#tupla[2] = 6
+#print(tupla)
+
+diccionario = {"uno": 10, "dos": True, "Tres": "tres", "Tres": False}
+print(diccionario)
+diccionario["Cuatro"] = 4
+print(diccionario)
+
+
+conjunto = {1,1,1,1,1,1,1,1,1,1,1}
+print(conjunto)
+
+print(type(conjunto))
+
+entero = 10
+print(type(entero))
+
+
+a = '1'
+b = '2'
+print(int(a) + int(b))
+
+cadena = "Holi Knoli"
+
+print(cadena[0:2])
+
+flotante = 12.83476587346
+otra_cadena = f"El valor {flotante:^20.3f} entero es {entero}"
+print(otra_cadena)
+
+
