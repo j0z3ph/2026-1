1f0d1245c99fa221c15f7a0f9bfff71a7c509b40
j0z3ph <j0z3ph@gmail.com>
2025-08-29 12:54:29 -0600

    Ejemplo

---
+++ IP 3/P1/programa.c
@@ -0,0 +1,40 @@
+/**
+ * @file programa.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C :>
+ * @version 0.1
+ * @date 2025-08-29
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include <stdio.h>
+
+int main()
+{
+    // Linea
+    /*char letra = getc(stdin); // hago una variable
+    putc(letra, stdout);
+    letra = getc(stdin);
+    putc(letra, stdout);
+    letra = getc(stdin);
+    putc(letra, stdout);
+    */
+    
+    /*char letra = getchar();
+    putchar(letra);
+    letra = getchar();
+    putchar(letra);
+    letra = getchar();
+    putchar(letra);
+    */
+
+
+    char cadena[20];
+    gets(cadena);
+    puts(cadena);
+
+    puts("Adios");
+    
+    return 0;
+}
