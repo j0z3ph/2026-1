85dfb45d42b49d4c598189bee7168c3e883d4191
Jose Luis Cruz <j0z3ph@gmail.com>
2025-08-29 18:12:50 -0600

    Ejemplo

---
+++ PE/P1/programa.c
@@ -0,0 +1,27 @@
+/**
+ * @file programa.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C :>
+ * @version 0.1
+ * @date 2025-08-29
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    /*
+    putc(getc(stdin), stdout);
+    putc(getc(stdin), stdout);
+    putc(getc(stdin), stdout);
+    */
+
+    putchar(getchar());
+    putchar(getchar());
+    putchar(getchar());
+    
+
+    return 0;
+}
