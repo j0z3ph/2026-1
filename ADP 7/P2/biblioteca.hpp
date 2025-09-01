3d553d14f9d363c4a4de7e913aab7bc5629c0076
Jose Luis Cruz <j0z3ph@gmail.com>
2025-09-01 16:03:29 -0600

    Ejemplos

---
+++ ADP 7/P2/biblioteca.hpp
@@ -0,0 +1,18 @@
+#ifndef __BIBLIOTECA_HPP__
+#define __BIBLIOTECA_HPP__
+//Declaraciones de funciones
+
+/// @brief Esta funcion suma dos enteros
+/// @param a Primer entero
+/// @param b Segundo entero
+/// @return La suma
+int suma(int a, int b);
+
+/// @brief Esta funcion suma tres flotantes
+/// @param a Primer flotante
+/// @param b Segundo flotante
+/// @param c Tercer flotante
+/// @return La suma
+float suma(float a, float b, float c = 0.0f, float d = 0.0f);
+
+#endif
---
+++ ADP 7/P2/principal.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file principal.cpp
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Un programa
+ * @version 0.1
+ * @date 2025-09-01
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include "biblioteca.hpp"
+
+using namespace std;
+
+int main()
+{
+    cout << suma(5.4f, 7.7f, 3.2f, 5.4f) << endl;
+    return 0;
+}
