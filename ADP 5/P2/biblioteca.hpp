85e5aae516c1eb82f97e60fbdcdac7c136d67863
Jose Luis Cruz <j0z3ph@gmail.com>
2025-09-02 14:22:51 -0600

    Biblioteca

---
+++ ADP 5/P2/biblioteca.hpp
@@ -0,0 +1,18 @@
+#ifndef __BIBLIOTECA_HPP__
+#define __BIBLIOTECA_HPP__
+ 
+// Declaraciones de funciones
+
+/// @brief Suma dos numeros enteros
+/// @param a El primer numero
+/// @param b El segundo numero
+/// @return La suma
+int suma(int a, int b);
+
+/// @brief Suma dos numeros flotantes
+/// @param a El primer numero
+/// @param b El segundo numero
+/// @return La suma
+float suma(float a, float b, float c = 0.0f, float d = 0.0f);
+
+#endif
---
+++ ADP 5/P2/programa.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file programa.cpp
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Ejemplo de biblioteca de funciones
+ * @version 0.1
+ * @date 2025-09-02
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include <iostream>
+#include <cstdlib>
+#include "biblioteca.hpp"
+
+using namespace std;
+
+int main()
+{
+    cout << suma(5.5f, 6.53f) << endl;
+
+    return 0;
+}
