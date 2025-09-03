a40b22fe0955b297c44a2c3d2f64c8f34a39db6a
Jose Luis Cruz <j0z3ph@gmail.com>
2025-09-02 18:50:34 -0600

    Ejemplos

---
+++ ADP 9/P2/programa.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file programa.cpp
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Ejemplo de biblioteca de funciones
+ * @version 0.1
+ * @date 2025-09-02
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include <iostream>
+#include <ctime>
+#include <cmath>
+#include <cstdlib>
+#include "biblioteca.hpp"
+
+using namespace std;
+
+int main()
+{
+    cout << suma(5.6f, 6.57f) << endl;
+
+    return 0;
+}
