c82702ce6e5e062dfe2f3ec6bbebc89257816497
Jose Luis Cruz <j0z3ph@gmail.com>
2025-09-04 21:50:42 -0600

    Programas

---
+++ P/P2/Programa.java
@@ -0,0 +1,16 @@
+/**
+ * Programa.java
+ * @author Yo Merengues
+ * Jugando con variables y constantes
+ * 4/9/2025
+ */
+
+ public class Programa {
+    final double PI = 3.1416;
+
+    public static void main(String[] args) {
+        int a = -10;
+
+        System.out.println(a >>> 1);
+    }
+ }
