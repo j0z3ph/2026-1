2696ee04adaf2cd476ab7db0bd2a06888b5ed180
Jose Luis Cruz <j0z3ph@gmail.com>
2025-09-05 13:00:30 -0600

    Mas codigo

---
+++ IP 3/P3/otro_programa.c
@@ -0,0 +1,20 @@
+/**
+ * @file otro_programa.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Jugando con numeros
+ * @version 0.1
+ * @date 2025-09-05
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    int numero = -36;
+
+    printf("%u\n", numero >> 1);
+    
+    return 0;
+}
