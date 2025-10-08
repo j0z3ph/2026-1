f841d33f81761531a593bcd28134b3ac872e3c70
Jose Luis Cruz <j0z3ph@gmail.com>
2025-10-07 21:39:53 -0600

    Simple controller

---
+++ PA 4/P12/digitalwrite.py
@@ -0,0 +1,14 @@
+import simplecontroller as sc
+import time 
+
+device = sc.Board("/dev/cu.usbmodem14101")
+device.pinMode(13, sc.OUTPUT)
+#device.pinMode(13, sc.INPUT_PULLUP)
+time.sleep(0.1)
+while True:
+    device.digitalWrite(13, True)
+    time.sleep(1)
+    device.digitalWrite(13, False)
+    time.sleep(1)
+    #device.digitalWrite(2, not device.digitalRead(13))
+    
