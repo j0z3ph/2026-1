05be4a44ab78ed0e4c7da343e0f6cb823c5956c3
Jose Luis Cruz <j0z3ph@gmail.com>
2025-11-14 13:30:54 -0600

    Uniones

--- IP 3/P20/uniones.c
+++ IP 3/P20/uniones.c
@@ -12,18 +12,18 @@
 
 struct Estructura
 {
-    int entero;
-    float flotante;
-    double doble;
-    char caracter;
+    int entero; // 4
+    float flotante; // 4
+    double doble; // 8
+    char caracter; // 1
 };
 
 union Union
 {
-    int entero;
-    float flotante;
-    double doble;
-    char caracter;
+    int entero; // 4
+    float flotante; // 4
+    double doble; // 8
+    char caracter; // 1
 };
 
 
@@ -35,11 +35,20 @@ int main()
     e.entero = 10;
     u.entero = 10;
 
-    printf("%i - %i\n", e.entero, u.entero);
+    printf("%i - %i - %lu\n", e.entero, u.entero, sizeof(struct Estructura));
 
     e.flotante = 3.14;
     u.flotante = 3.14;
-    printf("%f - %f\n", e.flotante, u.flotante);
+    printf("%f - %f - %lu\n", e.flotante, u.flotante, sizeof(union Union));
+
+    e.doble = 3.14;
+    u.doble = 3.14;
+    printf("%lf - %lf - %lu\n", e.doble, u.doble, sizeof(union Union));
+
+    printf("%i - %i - %lu\n", e.entero, u.entero, sizeof(struct Estructura));
+
+    printf("%lf - %lf - %lu\n", e.doble, u.doble, sizeof(union Union));
+
 
     
     return 0;
