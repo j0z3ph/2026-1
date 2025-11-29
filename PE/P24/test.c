01e391c5a55d2dc628cf0cb91c47bb672d2c8ea1
j0z3ph <j0z3ph@gmail.com>
2025-11-28 19:15:48 -0600

    Serial

---
+++ PE/P24/test.c
@@ -0,0 +1,28 @@
+/**
+ * @file test.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer uso de la simple controller
+ * @version 0.1
+ * @date 2025-11-28
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include "simplecontroller.h"
+
+int main()
+{
+    Board *b = connectDevice("COM6", B115200);
+    b->pinMode(b, 2, OUTPUT);
+    
+    while (1)
+    {
+        b->digitalWrite(b, 2, 1),
+        Sleep(1000);
+        b->digitalWrite(b, 2, 0),
+        Sleep(1000);
+    }
+
+    b->closeDevice(b);
+    return 0;
+}
